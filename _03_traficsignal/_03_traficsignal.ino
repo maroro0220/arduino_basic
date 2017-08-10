3c22afda05d9571a415372cb9c81315919c95eb8
maroro0220 <30305125+maroro0220@users.noreply.github.com>
2017-08-10 17:01:19 +0900

    traffic signal
    led control

---
+++ _03_traficsignal/_03_traficsignal.ino
@@ -0,0 +1,34 @@
+int REDpin=6;
+int YELLOWpin=5;
+int GREENpin=4;
+int BLUEpin=7;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(REDpin,OUTPUT);
+pinMode(YELLOWpin,OUTPUT);
+pinMode(GREENpin,OUTPUT);
+pinMode(BLUEpin,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+int i=0;
+int count=4;
+for(i;i<8;i++){
+if(i<=3){
+digitalWrite(count,HIGH);
+delay(500);
+digitalWrite(count,LOW);
+count++;
+}
+else{
+  if(i==4)
+  count--;
+count--;
+digitalWrite(count,HIGH);
+delay(500);
+digitalWrite(count,LOW);
+
+}
+}
+}
