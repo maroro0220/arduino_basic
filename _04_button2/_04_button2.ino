b0e878afeb2b46ea5e056a4ca3f0e4c8ff2a5daf
maroro0220 <30305125+maroro0220@users.noreply.github.com>
2017-11-08 12:32:51 +0900

    Add files via upload

--- _04_button2/_04_button2.ino
+++ _04_button2/_04_button2.ino
@@ -1,4 +1,4 @@
-const int LED=9;     //9번 핀을 사용하는 LED 상수 정의
+const int LED=11;     //9번 핀을 사용하는 LED 상수 정의
 const int BUTTON=2;   //버튼을 2번 핀에 지정
 
 boolean lastButton=LOW;   //이전 버튼의 눌림 상태를 Boolean형 변수로 선언
@@ -10,8 +10,6 @@ void setup()
     pinMode(LED, OUTPUT);   //LED(9번 핀)를 출력으로 지정
     pinMode(BUTTON, INPUT); //BUTTON(2번 핀)을 입력으로 지정
 }
-
-
 /*
 * 스위치 디바운싱 함수
 * debounce() 함수에 이전 버튼 상태를 인수로 사용하여
@@ -34,7 +32,7 @@ boolean debounce(boolean last)
 void loop()
 {
     currentButton=debounce(lastButton); //디바운싱 처리된 버튼값을 읽음
-    if(lastButton= =LOW&&currentButton= =HIGH) //버튼을 누름
+    if(lastButton==LOW && currentButton==HIGH) //버튼을 누름
     {
         ledOn=!ledOn; //LED 점멸 상태를 나타내는 ledOn 변수값을 바꿈
     }
